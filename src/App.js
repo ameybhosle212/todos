c80072a328217b123da60fd0d9ece72cf7443eb6
“ameybhosle212” <ameybhosle212@gmail.com>
2022-01-27 16:48:01 +0530

    Commited2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [title , setTitle] = useState('')
   const [todos , setTodos] = useState([])
+  const [edi , setEdi ] = useState(0);
+  const [changedValue , setChangedValue ] = useState('');
+  const [indexed , setIndexed] = useState(0);
+  useEffect(()=>{
+    // console.log(todos);
+  },[edi])
   const Submit = (e)=>{
     e.preventDefault();
     setTodos([...todos , title]);
@@ -13,23 +19,50 @@ function App() {
     todos.splice(index,1);
     setTodos([...todos]);
   }
+  const Edit = (index)=>{
+    var d = todos.splice(index,1);
+    setChangedValue(d);
+    setIndexed(index);
+    console.log(todos);
+    setEdi(1);
+  }
+  const Edited = ()=>{
+    todos.splice(indexed,0, changedValue)
+    // todos[indexed] = changedValue ;
+    setTodos([...todos])
+    console.log(todos);
+    setChangedValue('')
+    setEdi(0);
+  }
   return (
     <div className="App">
-      <div className=''>
-        <form onSubmit={Submit}>
-          ADD TODO:
-          <input className='border-2 border-black mx-4' name='todo' value={title} onChange={e => setTitle(e.target.value)} />
-          <button className='bg-green-600 border-black border-2 mx-4'>Submit</button>
-        </form>
-      </div>
+      {edi ? 
+        <div>
+          <form>
+            Edit Todo:
+            <input name='edit' value={changedValue} onChange={e => setChangedValue(e.target.value)} />
+            <button onClick={Edited}>Set Todo</button>
+          </form>
+        </div>
+        : 
+        <div className=''>
+          <form onSubmit={Submit}>
+            ADD TODO:
+            <input className='border-2 border-black mx-4' name='todo' value={title} onChange={e => setTitle(e.target.value)} />
+            <button className='bg-green-600 border-black border-2 mx-4'>Submit</button>
+          </form>
+        </div>}
       <div>
         {todos.map((val,index)=>{
-          return(
-            <div className='mx-4 my-4 border-2 border-black'>
-              <h1>{index+1} : {val}</h1>
-              <button className='bg-red-500 border-black border-2 mx-4 my-2' onClick={ () => deleted(index)}>Delete</button>
-            </div>
-          );
+          if(val != ""){
+            return(
+              <div className='mx-4 my-4 border-2 border-black'>
+                <h1>{index+1} : {val}</h1>
+                <button className='bg-green-600 border-black border-2 mx-4' onClick={() => Edit(index)}>Edit</button>
+                <button className='bg-red-500 border-black border-2 mx-4 my-2' onClick={ () => deleted(index)}>Delete</button>
+              </div>
+            );
+          }
         })}
       </div>
     </div>
